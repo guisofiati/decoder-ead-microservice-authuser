ceffafdbc2177475ee480b5d53618d3eaf97801c
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-09-21 10:39:46 -0300

    setup Retry Resilience4j and timeout with Spring Cloud Circuit Breaker

--- src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
+++ src/main/java/com/ead/authuser/configs/RestTemplateConfig.java
@@ -6,12 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestTemplateConfig {
+
+    static final int TIMEOUT = 5000;
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofMillis(TIMEOUT)) // if course delay more than 5 secs to answer will throw exception
+                .setReadTimeout(Duration.ofMillis(TIMEOUT))
+                .build();
     }
 }
