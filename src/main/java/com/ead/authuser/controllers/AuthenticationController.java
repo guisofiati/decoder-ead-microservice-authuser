efbf6490b4ed1ce115ac5616be369ad67088bc92
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-11-10 09:02:37 -0300

    password encoder and user roles

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -1,9 +1,12 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.UserDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.RoleModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.log4j.Log4j2;
@@ -11,6 +14,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +30,12 @@ public class AuthenticationController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
                                                @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
@@ -34,18 +44,20 @@ public class AuthenticationController {
             log.warn("[POST] >> method 'registerUser'. Username {} is already taken. ", userDto.getUsername());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is already taken.");
         }
-
         if(userService.existsByEmail(userDto.getEmail())) {
             log.warn("[POST] >> method 'registerUser'. E-mail {} is already taken. ", userDto.getEmail());
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: E-mail is already taken.");
         }
+        RoleModel roleModel = roleService.findByRoleName(RoleType.ROLE_STUDENT).orElseThrow(() -> new RuntimeException("Error: Role not found"));
 
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         userModel.setUserStatus(UserStatus.ACTIVE);
         userModel.setUserType(UserType.STUDENT);
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+        userModel.getRoles().add(roleModel);
         userService.saveUser(userModel);
         log.debug("[POST] >> method 'registerUser'. userId saved {} ", userModel.getUserId());
         log.info("User saved successfully. userId {} ", userModel.getUserId());
