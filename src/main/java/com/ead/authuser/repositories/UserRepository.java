1caa3eb13e30e673eb040fadfff0b659e34c0543
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2023-11-13 09:21:23 -0300

    basic auth with database - userDetails

--- src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -1,13 +1,18 @@
 package com.ead.authuser.repositories;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID>, JpaSpecificationExecutor<UserModel> {
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    @EntityGraph(attributePaths = "roles", type = EntityGraph.EntityGraphType.FETCH) // needed due to entity relation with roles was early declared as LAZY
+    Optional<UserModel> findByUsername(String username);
 }
